065ce7a0c5f7658799215c1f3158e75bb1163fa0
Joshua Klein <mobiusklein@gmail.com>
2015-04-10 13:13:19 -0400

    Debug RTD

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,6 +23,7 @@ import os
 sys.path.insert(0, os.path.abspath('ext'))
 # Module path
 sys.path.insert(0, os.path.abspath('../..'))
+print(sys.path)
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
