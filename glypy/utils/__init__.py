c56605eb2c04a98b8429a223e9775662ce02eb3a
Joshua Klein <mobiusklein@gmail.com>
2015-08-18 09:27:54 -0400

    Adjusted import paths;

--- glypy/utils/__init__.py
+++ glypy/utils/__init__.py
@@ -1,27 +1,8 @@
 import pkg_resources
-try:  # pragma: no cover
-    import cPickle as pickle
-except:  # pragma: no cover
-    import pickle
-try:  # pragma: no cover
-    from lxml import etree as ET
-except ImportError:  # pragma: no cover
-    try:
-        from xml.etree import cElementTree as ET
-    except:
-        from xml.etree import ElementTree as ET
-
-try:   # pragma: no cover
-    from cStringIO import StringIO
-except:  # pragma: no cover
-    try:
-        from StringIO import StringIO
-    except:
-        from io import StringIO
 
 from .base import (opener, make_counter, invert_dict, identity,
                    nullop, chrinc, make_struct, classproperty, cyclewarning,
-                   root, tree, groupby)
+                   root, tree, groupby, pickle, ET, StringIO)
 
 __all__ = ['opener', 'make_counter', 'invert_dict', 'identity', 'nullop',
            "chrinc", "make_struct", "classproperty", "cyclewarning",
--- glypy/utils/base.py
+++ glypy/utils/base.py
@@ -3,7 +3,25 @@ import sys
 import gzip
 
 from collections import defaultdict
+try:  # pragma: no cover
+    import cPickle as pickle
+except:  # pragma: no cover
+    import pickle
+try:  # pragma: no cover
+    from lxml import etree as ET
+except ImportError:  # pragma: no cover
+    try:
+        from xml.etree import cElementTree as ET
+    except:
+        from xml.etree import ElementTree as ET
 
+try:   # pragma: no cover
+    from cStringIO import StringIO
+except:  # pragma: no cover
+    try:
+        from StringIO import StringIO
+    except:
+        from io import StringIO
 
 def cyclewarning():
     '''
