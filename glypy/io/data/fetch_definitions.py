45297c7a9f01610f31e99781075226a084e02663
Joshua Klein <mobiusklein@gmail.com>
2018-08-02 12:35:05 -0400

    update GlycoRDF version

--- glypy/io/data/fetch_definitions.py
+++ glypy/io/data/fetch_definitions.py
@@ -2,5 +2,5 @@ from rdflib import Graph
 
 g = Graph()
 g.bind('glycan', "http://purl.jp/bio/12/glyco/glycan#")
-g.parse("https://raw.githubusercontent.com/ReneRanzinger/GlycoRDF/master/ontology/glycan.owl", format="turtle")
-g.serialize(open("glyspace.ttl", "wb"), 'turtle')
+g.parse("https://raw.githubusercontent.com/ReneRanzinger/GlycoRDF/master/ontology/glycan.ttl", format="turtle")
+g.serialize(open("glycordf.ttl", "wb"), 'turtle')
