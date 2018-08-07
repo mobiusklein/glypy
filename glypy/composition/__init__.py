4da4a4fafa175e470f0efe3ec6ce3d02589ea06a
Joshua Klein <mobiusklein@gmail.com>
2018-08-07 07:26:17 -0400

    Remove old __package__ assignment

--- glypy/composition/__init__.py
+++ glypy/composition/__init__.py
@@ -1,6 +1,9 @@
-__all__ = ["composition"]
-__package__ = "glypy.composition"
-
 from . import composition
 from .composition import Composition, calculate_mass
 from .base import formula, ChemicalCompositionError
+
+__all__ = [
+    "composition", "Composition", "calculate_mass",
+    "formula", "ChemicalCompositionError",
+    "composition_transform"
+]
