09584c2b94c60ac59f3cc1a3dfcb4a876053fdc1
Joshua Klein <mobiusklein@gmail.com>
2021-01-29 21:01:05 -0500

    remove outdated unused packaging extras

--- contrib/search/glypy/search/spectra/constants.py
+++
@@ -1,6 +0,0 @@
-from argparse import Namespace
-
-constants = Namespace()
-constants.MAX_PRECURSOR_CHARGE_STATE = 10
-constants.MAX_FRAGMENT_CHARGE_STATE = 4
-constants.MACHINE_ACQUISITION_RANGE = 3200 * 24
